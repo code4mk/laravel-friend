476270e287bcad19c99ed9271ee93c02f3e57488
Mostafa Kamal <hiremostafa@gmail.com>
2023-05-15 18:26:15 +0600

    start db

--- start_db.sh
+++ start_db.sh
@@ -1,5 +1,7 @@
+# https://github.com/code4mk/compose-hub/tree/main/db/mysql-phpmyadmin
 curl -o db_compose.yml -LJO https://raw.githubusercontent.com/code4mk/compose-hub/main/db/mysql-phpmyadmin/compose.yml
 
+# compose up
 docker compose \
   --file db_compose.yml \
   up -d
